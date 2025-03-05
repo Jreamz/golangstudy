276baf37284b63e88d05657a0f4dae754e7f1d69
Mason Putney <mputney38n@nextgenid.com>
2025-03-04 18:26:32 -0700

    fixed countConnections so working

--- countconnections.go
+++ countconnections.go
@@ -2,11 +2,20 @@ package main
 
 func countConnections(groupSize int) int {
 	totalConnections := 0
-	for num := 0; num <= groupSize; num++ {
-		if num == 1 {
-			break
+
+	for num := 1; num <= groupSize; num++ {
+		newConnections := num - 1
+		if groupSize == 1 {
+			return totalConnections
 		}
-		totalConnections += num * num / groupSize
+		totalConnections += newConnections
 	}
 	return totalConnections
 }
+
+/*
+Complete the countConnections function that takes an integer groupSize representing the number of people in the group
+chat and returns an integer representing the number of connections between them. For each additional person in the
+group, the number of new connections continues to grow. Use a for loop to accumulate the number of connections instead
+of directly using a mathematical formula.
+*/
