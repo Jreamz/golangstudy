110aadd8540135541fa66017522bcb22d85b7b27
Mason Putney <mputney38n@nextgenid.com>
2025-03-11 00:46:04 -0600

    ch8 slices, l6 make

--- makestaticslice.go
+++ makestaticslice.go
@@ -9,3 +9,10 @@ func getMessageCosts(messages []string) []float64 {
 	}
 	return messagesTotal
 }
+
+/*
+Complete the getMessageCosts() function. It takes a slice of messages and returns a slice of message costs.
+    Preallocate a slice for the message costs of the same length as the messages slice.
+    Fill the costs slice with costs for each message. The cost in the cost slice should correspond to the message
+in the messages slice at the same index. The cost of a message is the length of the message multiplied by 0.01.
+*/
